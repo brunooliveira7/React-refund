d45caa1edc7737dcbcd9cb71a7984605bea36688
Bruno Oliveira <brunoramosoliveira7@icloud.com>
2025-06-11 09:59:14 -0300

    feat: Implement SignUp page and route
    This commit introduces the SignUp page with form fields for name, email, password, and confirm password. It also adds a route to access the SignUp page.
    
    - Created `src/pages/SingUp.tsx` with input fields for user registration.
    - Implemented state management for form inputs (name, email, password, confirmPassword).
    - Added a route for the SignUp page in `src/routes/AuthRoutes.tsx`.
    - Renamed `auth-routes.tsx` to `AuthRoutes.tsx` and updated the import in `src/routes/index.tsx`.

--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router";
 import { SignIn } from "../pages/SignIn";
 import { AuthLayout } from "../components/AuthLayout";
+import { SignUp } from "../pages/SingUp";
 
 export function AuthRoutes() {
   return (
     <Routes>
       <Route path="/" element={<AuthLayout />}>
         <Route path="/" element={<SignIn />} />
+        <Route path="/signUp" element={<SignUp />} />
       </Route>
 
     </Routes>
