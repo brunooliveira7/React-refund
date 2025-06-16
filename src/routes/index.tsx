7c29538ab3bb7774f2eb81990904244ee70b1fe8
Bruno Oliveira <brunoramosoliveira7@icloud.com>
2025-06-16 15:09:16 -0300

    feat: Implement role-based routing
    This commit implements role-based routing to display different components based on the user's role.
    
    - Added a `session` object with a `user` property containing the user's role.
    - Created a `Route` component that renders different routes based on the `session.user.role` value.
    - Replaced the hardcoded `<AuthRoutes />` component with the `<Route />` component within the `<BrowserRouter>`.
    - Set `isLoading` to `false` to disable the loading screen.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,16 +4,35 @@ import { EmployeeRoutes } from "./EmployeeRoutes";
 import { ManagerRoutes } from "./ManagerRoutes";
 import { Loading } from "../components/Loading";
 
-const isLoading = true;
+const isLoading = false;
+
+//const session = undefined
+
+const session = {
+  user: {
+    role: "employee",
+  },
+};
 
 export function Routes() {
+  function Route() {
+    switch (session?.user.role) {
+      case "employee":
+        return <EmployeeRoutes />;
+      case "manager":
+        return <ManagerRoutes />;
+      default:
+        return <AuthRoutes />;
+    }
+  }
+
   if (isLoading) {
     return <Loading />;
   }
 
   return (
     <BrowserRouter>
-      <AuthRoutes />
+      <Route />
     </BrowserRouter>
   );
 }
